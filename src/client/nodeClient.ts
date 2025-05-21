0e4371644fbf007e9eccfc9eafd73802d34a9aee
Raffael Campos <raffael.rcampos@gmail.com>
2025-05-21 17:56:28 -0300

    fix: update Pool import to use type syntax and refactor getPool method to require pg dynamically

--- src/client/nodeClient.ts
+++ src/client/nodeClient.ts
@@ -1,7 +1,7 @@
 import { EnvironmentType } from "@kwilteam/kwil-js/dist/core/enums";
 import { NodeKwil } from "@kwilteam/kwil-js";
 import { BaseTNClient, TNClientOptions } from "./client";
-import { Pool } from "pg";
+import { type Pool } from "pg";
 export class NodeTNClient extends BaseTNClient<EnvironmentType.NODE> {
   constructor(options: TNClientOptions) {
     super(options);
@@ -12,6 +12,8 @@ export class NodeTNClient extends BaseTNClient<EnvironmentType.NODE> {
   }
 
   protected getPool(): Pool | undefined {
+    const pg = require("pg");
+    const { Pool } = pg;
     if (this.neonConnectionString) {
       return new Pool({ connectionString: this.neonConnectionString });
     }
